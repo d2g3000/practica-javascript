afd2b7a5748f58aef3189fd45f4d283f25d1af8c
d2g3000 <d2g3000@gmail.com>
2022-11-30 10:41:20 -0400

    primer commit

---
+++ variables/codigojs.js
@@ -0,0 +1,15 @@
+'use strict';
+var a = 1;//numericos
+var b = 'cadena';//cadena de caracteres
+var c = 10.5;//decimal
+var d = true;//boleano
+var e = ['h','k','l','i'];//arreglos
+var f = {nombre:'Danny',edad:43,nacionalidad:'Venezolano'};// objetos
+
+console.log(` tipo de variable ${typeof(a)} ${a} `)
+console.log(` tipo de variable ${typeof(b)} ${b} `)
+console.log(` tipo de variable ${typeof(c)} ${c} `)
+console.log(` tipo de variable ${typeof(d)} ${d} `)
+console.log(` tipo de variable ${typeof(e)} ${e} `)
+console.log(` tipo de variable ${typeof(f)} ${f} `)
+
